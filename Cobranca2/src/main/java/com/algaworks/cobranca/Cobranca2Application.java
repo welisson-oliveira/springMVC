ba7ffa06c41cdcef56a8dc23954f28f2619c3a9b
ROSANGELA <ROSANGELA@ROSANGELA-PC.home>
2016-12-30 20:11:21 -0200

    subindo projeto correto

---
+++ Cobranca2/src/main/java/com/algaworks/cobranca/Cobranca2Application.java
@@ -0,0 +1,12 @@
+package com.algaworks.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cobranca2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cobranca2Application.class, args);
+	}
+}
