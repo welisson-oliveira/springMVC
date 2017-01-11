691e564de783ca2e0ea0bf6de365f4ede8d1ce10
Focus <Focus@192.168.15.40>
2017-01-11 15:04:26 -0200

    projeto finalizado.

--- Cobranca2/src/main/java/com/algaworks/cobranca/Cobranca2Application.java
+++ Cobranca2/src/main/java/com/algaworks/cobranca/Cobranca2Application.java
@@ -2,11 +2,28 @@ package com.algaworks.cobranca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Cobranca2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cobranca2Application.class, args);
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			// TODO Auto-generated method stub
+			super.addViewControllers(registry);
+			
+			registry.addRedirectViewController("/", "/titulos");
+			
+		}
+		
 	}
 }
